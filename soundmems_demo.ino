6897c4ce484086a494c077b212fe6818bd2c20b7
paulmorley <45638888+paulmorley@users.noreply.github.com>
2018-12-05 13:46:32 -0600

    Update soundmems_demo.ino

--- soundmems_demo.ino
+++ soundmems_demo.ino
@@ -74,7 +74,7 @@ Notes:
 #define LED_TYPE WS2812B                                                         // Using APA102, WS2812, WS2801. Don't forget to modify LEDS.addLeds to suit.
 #define NUM_LEDS 128                                                       // Number of LED's.
 
-//testing pleasse accept
+//testing please accept
 
 
 // Fixed sound hardware definitions cannot change on the fly.
